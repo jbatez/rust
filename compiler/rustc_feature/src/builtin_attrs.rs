fa8d109639c62d2034d97c85469f329b5e091524
Jo Bates <29763794+jbatez@users.noreply.github.com>
2025-05-21 04:29:40 -0700

    Mark define_in_every_cgu_used attribute as unsafe

--- compiler/rustc_feature/src/builtin_attrs.rs
+++ compiler/rustc_feature/src/builtin_attrs.rs
@@ -619,9 +619,9 @@ pub static BUILTIN_ATTRIBUTES: &[BuiltinAttribute] = &[
         EncodeCrossCrate::Yes, min_generic_const_args, experimental!(type_const),
     ),
 
-    // `#[define_in_every_cgu_used]`
+    // `#[unsafe(define_in_every_cgu_used)]`
     gated!(
-        define_in_every_cgu_used, Normal, template!(Word), ErrorPreceding,
+        unsafe define_in_every_cgu_used, Normal, template!(Word), ErrorPreceding,
         EncodeCrossCrate::No, experimental!(define_in_every_cgu_used),
     ),
 
